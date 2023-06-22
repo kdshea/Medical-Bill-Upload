4c33f0c3adbc66f956059317ffdeaaff979b3d4f
Kate Shea <daishea@gmail.com>
2023-06-22 07:22:06 +0530

    Updated db url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api'
-const PROD_API_URL = process.env.REACT_APP_API_URL
-// const API_URL = '/api'
+const API_URL = process.env.REACT_APP_API_URL
 
 
 export default API_URL
